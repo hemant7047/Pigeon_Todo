8627a13ad7ad700795f8ff677bae7410d240c9b1
Hemant <hsr7047@gmail.com>
2021-11-19 15:45:23 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Project CC';
+  
+  todoValue:string="";
+  list:Todo[]=[];
+
+  ngOnInit(){
+    this.list=[];
+    this.todoValue="";
+  }
+
+  addItem(){
+    if(this.todoValue!==""){
+      const newItem:Todo={
+        id:Date.now(),
+        value:this.todoValue,
+        isDone:false
+      };
+
+      this.list.push(newItem);
+    }
+        this.todoValue="";
+  }
+
+    deleteItem(id:number){
+        this.list=this.list.filter(item=>item.id !==id);
+    }
+
+
+
+
+
+
+
+
+
+
 }
